1fd06b712e49f20f59d270e85b79f0ec320dd6fc
youthlin.chen <youthlin.chen@qunar.com>
2017-11-09 10:44:00 +0800

    cglib

---
+++ example-cglib/src/main/java/com/youthlin/example/cglib/Main.java
@@ -0,0 +1,22 @@
+package com.youthlin.example.cglib;
+
+import com.youthlin.example.cglib.bean.FiledRecorder;
+import com.youthlin.example.cglib.bean.User;
+
+import java.math.BigDecimal;
+
+/**
+ * 创建: youthlin.chen
+ * 时间: 2017-11-09 10:14
+ */
+public class Main {
+    public static void main(String[] args) {
+        FiledRecorder<User> userFiledRecorder = new FiledRecorder<>(User.class);
+        User user = User.newUser(userFiledRecorder);
+        System.out.println(userFiledRecorder.getFieldStatusMap());
+        System.out.println(user);
+        user.setAmount(BigDecimal.ONE);
+        System.out.println(userFiledRecorder.getFieldStatusMap());
+        System.out.println(user);
+    }
+}
