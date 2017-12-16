7048db77064dc568fd668fdb87b34348977360be
youthlin.chen <youthlin.chen@qunar.com>
2017-12-16 16:10:26 +0800

    bootstrap

--- example-rpc-provider/src/main/java/com/youthlin/example/rpc/provider/Provider.java
+++ example-rpc-provider/src/main/java/com/youthlin/example/rpc/provider/Provider.java
@@ -2,13 +2,11 @@ package com.youthlin.example.rpc.provider;
 
 import com.youthlin.ioc.context.ClasspathContext;
 import com.youthlin.ioc.context.Context;
-import com.youthlin.ioc.spi.IPostScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.ServiceLoader;
 
 /**
  * 创建: youthlin.chen
@@ -18,7 +16,6 @@ public class Provider {
     private static final Logger LOGGER = LoggerFactory.getLogger(Provider.class);
 
     public static void main(String[] args) throws IOException {
-        //ServiceLoader<IPostScanner> postScanners = ServiceLoader.load(IPostScanner.class);
         Context context = new ClasspathContext("com.youthlin.example");
         Map<Class, Object> beanMap = context.getClazzBeanMap();
         LOGGER.info("{}", beanMap);
