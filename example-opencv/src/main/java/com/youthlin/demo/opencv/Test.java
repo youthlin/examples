9ea24e05f1abc2d2d8b1ba5cc62fefdfe330ebaa
youthlin.chen <youthlin.chen@qunar.com>
2018-05-05 11:47:59 +0800

    opencv

--- example-opencv/src/main/java/com/youthlin/demo/opencv/Test.java
+++ example-opencv/src/main/java/com/youthlin/demo/opencv/Test.java
@@ -2,13 +2,23 @@ package com.youthlin.demo.opencv;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.highgui.HighGui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.VideoWriter;
+import org.opencv.videoio.Videoio;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 
 /**
  * 创建: youthlin.chen
@@ -19,10 +29,19 @@ public class Test {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
+    private static CascadeClassifier faceDetector = new CascadeClassifier();
+    private static CascadeClassifier eyeDetector = new CascadeClassifier();
+    private static final String xmlPath = "/Users/youthlin.chen/IdeaProjects/opencv-3.4.1/data/haarcascades/";
+
+    static {
+        faceDetector.load(xmlPath + "haarcascade_frontalface_alt.xml");
+        eyeDetector.load(xmlPath + "haarcascade_eye.xml");
+    }
+
     private static boolean stop = false;
-    private static boolean inited = false;
     private static JFrame window = new JFrame("OpenCV Test");
     private static JLabel label = new JLabel();
+    private static JLabel text = new JLabel();
 
     public static void main(String[] args) {
         EventQueue.invokeLater(Test::createFrameAndShow);
@@ -39,7 +58,9 @@ public class Test {
             }
         });
         window.setBounds(100, 100, 1280, 720);
-        capBtn.setBounds(33, 13, 113, 27);
+        text.setBounds(30, 0, 100, 10);
+        capBtn.setBounds(30, 10, 100, 30);
+        window.getContentPane().add(text);
         window.getContentPane().add(capBtn);
         label.setBounds(0, 0, 1280, 720);
         window.getContentPane().add(label);
@@ -54,24 +75,70 @@ public class Test {
             label.setText("摄像头不可用");
             return;
         }
+        final double fps = camera.get(Videoio.CV_CAP_PROP_FPS);
+        int height = (int) camera.get(Videoio.CAP_PROP_FRAME_HEIGHT);
+        int width = (int) camera.get(Videoio.CAP_PROP_FRAME_WIDTH);
+        EventQueue.invokeLater(() -> {
+            text.setText(String.format("%.2f", fps));
+            window.setSize(width, height);
+            label.setSize(width, height);
+            window.setResizable(false);
+        });
+        VideoWriter writer = new VideoWriter();
+        File out = new File("cap.mp4");
+        if (out.exists()) {
+            out.renameTo(new File("cap.renamedAt" + System.currentTimeMillis() + ".mp4"));
+            out.delete();
+        }
+        writer.open(out.getAbsolutePath(), VideoWriter.fourcc('P', 'I', 'M', '1'), fps, new Size(width, height));
+        if (!writer.isOpened()) {
+            writer = null;
+        }
         Mat pic = new Mat();
         while (!stop) {
             camera.read(pic);
-            label.setIcon(new ImageIcon(HighGui.toBufferedImage(pic)));
-            if (!inited) {
-                EventQueue.invokeLater(() -> {
-                    window.setSize(pic.width(), pic.height());
-                    label.setSize(pic.width(), pic.height());
-                    window.setResizable(false);
-                });
-                inited = true;
+            Mat face = detectFace(pic);
+            label.setIcon(new ImageIcon(HighGui.toBufferedImage(face)));
+            if (writer != null) {
+                writer.write(face);
             }
+            face.release();
+            pic.release();
             try {
                 Thread.sleep(100);//线程暂停100ms
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        camera.release();
+        if (writer != null) {
+            writer.release();
+        }
         System.out.println(pic.size());
     }
+
+    private static Mat detectFace(Mat mat) {
+        MatOfRect faces = new MatOfRect();
+        faceDetector.detectMultiScale(mat, faces);
+        for (Rect rect : faces.toArray()) {
+            Imgproc.rectangle(mat,
+                    new Point(rect.x, rect.y),
+                    new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(255, 0, 0));
+        }
+        eyeDetector.detectMultiScale(mat, faces);
+        for (Rect rect : faces.toArray()) {
+            Imgproc.circle(mat, new Point(rect.x + rect.width / 2, rect.y + rect.height / 2),
+                    rect.width / 4 + rect.height / 4, new Scalar(0, 0, 255));
+        }
+
+        return flip(mat);
+    }
+
+    private static Mat flip(Mat mat) {
+        Mat result = new Mat();
+        Core.flip(mat, result, 1);//0上下 1左右 -1上下再左右
+        return result;
+    }
+
 }
