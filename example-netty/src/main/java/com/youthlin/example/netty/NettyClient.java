cc0f6325905542dfdc7777d4c0a05a6c5a3eb3b8
youthlin.chen <youthlin.chen@qunar.com>
2018-11-01 19:04:45 +0800

    netty client server simple handler

--- example-netty/src/main/java/com/youthlin/example/netty/NettyClient.java
+++ example-netty/src/main/java/com/youthlin/example/netty/NettyClient.java
@@ -1,14 +1,17 @@
 package com.youthlin.example.netty;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.AttributeKey;
 
+import java.nio.charset.Charset;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -25,10 +28,10 @@ public class NettyClient {
         bootstrap.group(group)                          //1. 线程模型
                 .channel(NioSocketChannel.class)        //2. IO 模型
                 .handler(                               //3. 处理逻辑
-                        new ChannelInitializer<Channel>() {
+                        new ChannelInitializer<SocketChannel>() {
                             @Override
-                            protected void initChannel(Channel channel) throws Exception {
-                                channel.pipeline().addLast(new StringEncoder());
+                            protected void initChannel(SocketChannel channel) throws Exception {
+                                channel.pipeline().addLast(new ClientHandler());
                             }
                         })
                 .attr(AttributeKey.newInstance("clientName"), "MyClient")
@@ -43,17 +46,47 @@ public class NettyClient {
     private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
         bootstrap.connect(host, port).addListener(future -> {
             if (future.isSuccess()) {
-                System.out.println(String.format("[%1$tT] connect success", new Date()));
+                log("connect success");
             } else if (retry <= 0) {
-                System.out.println(String.format("[%1$tT] give up", new Date()));
+                log("give up");
             } else {
                 int order = MAX_RETRY - retry + 1;
                 int delay = 1 << order;
-                System.err.println(String.format("[%1$tT] %2$d: connect fail", new Date(), order));
+                err("connect fail");
                 bootstrap.config()
                         .group()
                         .schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
             }
         });
     }
+
+    private static class ClientHandler extends ChannelInboundHandlerAdapter {
+        @Override//连接建立成功之后调用
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            log("客户端写出数据");
+            ByteBuf buf = getByteBuf(ctx);
+            ctx.channel().writeAndFlush(buf);
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf buf = (ByteBuf) msg;
+            log("client read:" + buf.toString(Charset.forName("UTF-8")));
+        }
+
+        private ByteBuf getByteBuf(ChannelHandlerContext context) {
+            ByteBuf buffer = context.alloc().buffer();
+            buffer.writeBytes("Hello, 世界".getBytes(Charset.forName("UTF-8")));
+            return buffer;
+        }
+    }
+
+    private static void log(String msg) {
+        System.out.println(String.format("[%1$tT] %2$s", new Date(), msg));
+    }
+
+    private static void err(String msg) {
+        System.err.println(String.format("[%1$tT] %2$s", new Date(), msg));
+    }
+
 }
--- example-netty/src/main/java/com/youthlin/example/netty/NettyServer.java
+++ example-netty/src/main/java/com/youthlin/example/netty/NettyServer.java
@@ -1,15 +1,18 @@
 package com.youthlin.example.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
-import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
 import io.netty.util.AttributeKey;
+
+import java.nio.charset.Charset;
+import java.util.Date;
 
 /**
  * 创建: youthlin.chen
@@ -27,21 +30,14 @@ public class NettyServer {
                         new ChannelInitializer<NioSocketChannel>() {         // NioSocketChannel 对应 Socket
                             @Override
                             protected void initChannel(NioSocketChannel ch) throws Exception {
-                                ch.pipeline().addLast(new StringDecoder());
-                                ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
-                                    @Override
-                                    protected void channelRead0(ChannelHandlerContext channelHandlerContext,
-                                            String msg) throws Exception {
-                                        System.out.println(msg);
-                                    }
-                                });
+                                ch.pipeline().addLast(new ServerHandler());
                             }
                         })
                 .handler(                               // handler 对应 服务端启动过程中(bind后)的逻辑 childHandler 用于处理新连接
                         new ChannelInitializer<NioServerSocketChannel>() {
                             @Override
                             protected void initChannel(NioServerSocketChannel ch) throws Exception {
-                                System.out.println("Starting...");
+                                log("Starting...");
                             }
                         })
                 .attr(AttributeKey.newInstance("myKey"), "myValue")
@@ -57,11 +53,29 @@ public class NettyServer {
     private static void bind(ServerBootstrap serverBootstrap, int port) {
         serverBootstrap.bind(port).addListener(future -> {
             if (future.isSuccess()) {
-                System.out.println("bind on port: " + port);
+                log("bind on port: " + port);
             } else {
                 bind(serverBootstrap, port + 1);
             }
         });
     }
 
+    private static class ServerHandler extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf buf = (ByteBuf) msg;
+            log("read data: " + buf.toString(Charset.forName("UTF-8")));
+            log("server write data...");
+            ByteBuf out = getByteBuf(ctx);
+            ctx.channel().writeAndFlush(out);
+        }
+
+        private ByteBuf getByteBuf(ChannelHandlerContext context) {
+            return context.alloc().buffer().writeBytes("Server 响应数据".getBytes(Charset.forName("UTF-8")));
+        }
+    }
+
+    private static void log(String msg) {
+        System.out.println(String.format("[%1$tT] %2$s", new Date(), msg));
+    }
 }
