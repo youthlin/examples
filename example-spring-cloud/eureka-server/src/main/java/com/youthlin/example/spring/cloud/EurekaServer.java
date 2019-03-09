5dfc6edaf1d864f474b759008393e44d14a1ad95
youthlin.chen <youthlin.chen@qunar.com>
2019-03-09 12:05:37 +0800

    eureka

---
+++ example-spring-cloud/eureka-server/src/main/java/com/youthlin/example/spring/cloud/EurekaServer.java
@@ -0,0 +1,19 @@
+package com.youthlin.example.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 创建: youthlin.chen
+ * 时间: 2019-03-08 21:44
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+
+}
