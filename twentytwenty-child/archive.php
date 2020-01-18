b638e443d8c598f55915fefb84659fedd8b352fe
youthlin.chen <yulinlin1995@gmail.com>
2020-01-18 16:04:36 +0800

    归档页面

--- twentytwenty-child/archive.php
+++ twentytwenty-child/archive.php
@@ -17,27 +17,25 @@
             </div>
         </header>
         <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-            <div class="post-inner">
-                <div class="entry-content archive-wrap">
-                    <?php
-                    add_filter('the_title', 'title_with_date', 10, 2);
-                    function title_with_date($title, $id) {
-                        global $post;
-                        $post = get_post($id);
-                        return $title . get_the_time('Y-m-d');
-                    }
+            <div class="entry-content">
+                <?php
+                add_filter('the_title', 'title_with_date', 10, 2);
+                function title_with_date($title, $id) {
+                    global $post;
+                    $post = get_post($id);
+                    return $title . get_the_time('Y-m-d');
+                }
 
-                    add_filter('get_archives_link', 're_format_link', 10, 7);
-                    function re_format_link($link_html, $url, $text, $format, $before, $after, $selected) {
-                        $date = substr($text, -10);
-                        $text = substr($text, 0, strlen($text) - 10);
-                        $link_html = "\t<li class='archive-item alignfull'>$before<span class='date meta-text'>$date</span> <a href='$url'>$text</a>$after</li>\n";
-                        return $link_html;
-                    }
+                add_filter('get_archives_link', 're_format_link', 10, 7);
+                function re_format_link($link_html, $url, $text, $format, $before, $after, $selected) {
+                    $date = substr($text, -10);
+                    $text = substr($text, 0, strlen($text) - 10);
+                    $link_html = "\t<li class='archive-item'>$before<span class='date meta-text'>$date</span> <a href='$url'>$text</a>$after</li>\n";
+                    return $link_html;
+                }
 
-                    wp_get_archives(array('type' => 'postbypost'));
-                    ?>
-                </div>
+                wp_get_archives(array('type' => 'postbypost'));
+                ?>
             </div>
         </article>
     </main>
