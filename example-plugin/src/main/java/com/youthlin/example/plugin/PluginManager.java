b2fa9de943d5fbfd46225c6cf76f41213caa69cb
youthlin.chen <yulinlin1995@gmail.com>
2020-04-23 16:15:47 +0800

    remove json dependency use properties instead.

--- example-plugin/src/main/java/com/youthlin/example/plugin/PluginManager.java
+++ example-plugin/src/main/java/com/youthlin/example/plugin/PluginManager.java
@@ -1,7 +1,8 @@
 package com.youthlin.example.plugin;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import lombok.Getter;
 
@@ -12,6 +13,7 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.Set;
 import java.util.jar.JarFile;
 import java.util.zip.ZipEntry;
@@ -21,7 +23,6 @@ import java.util.zip.ZipEntry;
  * @date 2020-04-15 17:41
  */
 public class PluginManager<T> {
-    private static final ObjectMapper MAPPER = new ObjectMapper();
     @Getter
     private final Set<PluginInstance<T>> instances = Sets.newHashSet();
 
@@ -43,13 +44,23 @@ public class PluginManager<T> {
     private PluginMeta getMeta(File file) throws IOException {
         InputStream is;
         if (file.isDirectory()) {
-            is = new FileInputStream(new File(file, "plugin.json"));
+            is = new FileInputStream(new File(file, "plugin.properties"));
         } else {
             JarFile jarFile = new JarFile(file);
-            ZipEntry metaEntry = jarFile.getEntry("plugin.json");
+            ZipEntry metaEntry = jarFile.getEntry("plugin.properties");
             is = jarFile.getInputStream(metaEntry);
         }
-        return MAPPER.readValue(is, PluginMeta.View.class).toMeta();
+        final Properties properties = new Properties();
+        properties.load(is);
+        final ImmutableMap<String, String> map = Maps.fromProperties(properties);
+        return new PluginMeta(
+                map.get("namespace"),
+                map.get("entrance"),
+                map.get("name"),
+                map.get("author"),
+                map.get("version"),
+                map.get("describe")
+        );
     }
 
     private URL[] fileToUrls(File file) throws MalformedURLException {
